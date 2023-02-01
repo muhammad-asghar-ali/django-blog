f606a8547201dad0202788f77bb56aa73edff174
apple <apple@apples-MacBook-Pro.local>
2023-02-02 01:07:03 +0500

    implement URL parameters and named URLs and name spacing

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
+# for multiple same names we use name-spacing
+
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list)
+    # Named URL's
+    path('', views.article_list, name="list"),
+    # URL Parameters
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail")
 ]
--- articles/views.py
+++ articles/views.py
@@ -5,3 +5,6 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all().order_by("date")
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    return HttpResponse()
