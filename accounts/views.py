ba80870e91556201c752f66b8992d19e94a1932a
apple <apple@apples-MacBook-Pro.local>
2023-02-08 00:32:33 +0500

    feat(): working on user creation form and save the user. fix(): fix the accounts url and make the blog working for accounts endpoints

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 # Create your views here.
 
+
 def signup_view(request):
-    return render(request, 'accounts/singup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {"form": form})
