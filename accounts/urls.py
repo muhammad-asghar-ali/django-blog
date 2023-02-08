50cc31c384ce1332952360c034e3b029ceea62e1
apple <apple@apples-MacBook-Pro.local>
2023-02-08 20:50:38 +0500

    feat(): add login and logout functionality and also add templates for it

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,7 @@ app_name = 'accounts'
 urlpatterns = [
     # Named URL's
     path('signup/', views.signup_view, name="signup"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import redirect
+from django.contrib.auth import login
 # Create your views here.
 
 
@@ -8,9 +9,31 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-
+            user = form.save()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {"form": form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()        
+            login(request, user)    
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {"form": form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()        
+            login(request, user)    
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {"form": form})
