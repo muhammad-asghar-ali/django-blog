9273a43e4724ccd0550a7c217a8ad55d28aea0fc
apple <apple@apples-MacBook-Pro.local>
2023-02-08 20:51:10 +0500

    feat(): add login and logout functionality and also add templates for it

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 # Create your views here.
 
 
@@ -29,11 +29,8 @@ def login_view(request):
 
 def logout_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()        
-            login(request, user)    
-            return redirect('articles:list')
+        logout(request)
+        return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {"form": form})
